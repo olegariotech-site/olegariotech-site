816d33e5f60b800bddeea2bf377f21f0006123d1
Olegario Tech <olegariotech.oficial@gmail.com>
2026-09-03 16:53:37 -0300

    feat: load OT V3 commercial layer

--- assets/js/ot-analytics.js
+++ assets/js/ot-analytics.js
@@ -13,6 +13,16 @@
   footerCss.rel='stylesheet';footerCss.href='/assets/css/ot-footer-v4.css?v=1';footerCss.id='ot-footer-v4-css';
   document.head.appendChild(footerCss);
 
+  var commercialCss=document.createElement('link');
+  commercialCss.rel='stylesheet';commercialCss.href='/assets/css/ot-commercial-v3.css?v=1';commercialCss.id='ot-commercial-v3-css';
+  document.head.appendChild(commercialCss);
+
+  function loadCommercial(){
+    var commercial=document.createElement('script');
+    commercial.src='/assets/js/ot-commercial-v3.js?v=1';commercial.async=false;
+    document.head.appendChild(commercial);
+  }
+
   var core=document.createElement('script');
   core.src='/assets/js/ot-analytics-core.js?v=2';core.async=false;
   core.onload=function(){
@@ -24,11 +34,14 @@
       mobile.onload=function(){
         var footer=document.createElement('script');
         footer.src='/assets/js/ot-footer-v4.js?v=1';footer.async=false;
+        footer.onload=loadCommercial;
+        footer.onerror=loadCommercial;
         document.head.appendChild(footer);
       };
       document.head.appendChild(mobile);
     };
     document.head.appendChild(v2);
   };
+  core.onerror=loadCommercial;
   document.head.appendChild(core);
 })();
